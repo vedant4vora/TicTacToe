d1091e4d00a4858cf10b3a3df89e14b17626baca
vedant4vora <104711846+vedant4vora@users.noreply.github.com>
2022-10-27 19:46:19 +0530

    Updated version

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ let isgameover = false;
 
 // Function to change the turn
 const changeTurn = ()=>{
-    return turn === "X"? "0": "X"
+    return turn === "X"? "O": "X"
 }
 
 // Function to check for a win
